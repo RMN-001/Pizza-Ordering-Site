859c42952199e7488e378b1454261e79a7c7b83b
Sarnyai Ákos <62949899+sarnyaia1@users.noreply.github.com>
2021-10-09 11:12:04 +0200

    add first file

---
+++ first.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1); ?>
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Basics 1 </title>
+</head>
+<body>
+    <h1>My first PHP page</h1>
+    <?php
+        echo "My first PHP script!";
+    ?>
+</body>
+</html>
